94bf8b6e8f6016de8cb07db9989ace9429a26f2b
csalas <c.salas@yubico.com>
2023-02-21 09:47:48 -0600

    revamped scripts for docker deployment / teardown

--- scripts/mysql/deploy_mysql.sh
+++ scripts/mysql/deploy_mysql.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
+# Generate a random value, primarily used for generating a new password
 function getRandomValue {
     echo $(ping -c 1 yubico.com |md5 | head -c$1; echo)
 }
 
 # Generating databse params
-echo "Generating database name, and master password"
-DATABASE_NAME=$(getParam DATABASE_NAME webauthnkitdb$SUFFIX | sed 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/') 
-DATABASE_MASTER_USERNAME=dbmaster
-DATABASE_MASTER_PASSWORD=$(getRandomValue 16)
+echo "Composing MYSQL database"
 
-if [ -e .env ]; then
-  echo "Removing old ENV file"
-  rm .env
+# Check if there is an ENV file
+# If no ENV file, generate an env file
+if [ ! -e .env ]; then
+  echo "Creating new Docker container"
+  echo "Generating database password"
+
+  # Use password provided by userer, or generate a random one
+  DATABASE_MASTER_PASSWORD=${1:-$(getRandomValue 16)}
+
+  echo "Database password created"
+
+  echo "Writing password to .env file"
+
+  echo "MASTER_PASSWORD=$DATABASE_MASTER_PASSWORD" >> .env
 fi
 
-echo "MASTER_PASSWORD=$DATABASE_MASTER_PASSWORD" >> .env
-# @TODO - Consider removing the env file, and printing the DB password once???
-
-docker compose up -d --wait
-
-echo "Waiting for the MySQL server to finish initializing"
-while ! docker exec -it passkey-mysql mysql -uroot -p$DATABASE_MASTER_PASSWORD -h 127.0.0.1 --protocol=TCP -e "status" &> /dev/null ; do
-    echo "Waiting for database connection..."
-    sleep 5
-done 
-
-docker exec -i passkey-mysql mysql -uroot -p$DATABASE_MASTER_PASSWORD -h 127.0.0.1 --protocol=TCP < db.sql 2>/dev/null | grep -v "mysql: [Warning] Using a password on the command line interface can be insecure."
+docker compose up -d
