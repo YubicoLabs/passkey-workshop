0c297282061f36b3caff3831f07c4e5e9937c1bf
Joost van Dijk <joost.vandijk@yubico.com>
2023-03-23 22:29:27 +0100

    change from json to bash config syntax

--- scripts/Mac-Linux/DeployProject.sh
+++ scripts/Mac-Linux/DeployProject.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Each parameter enters here. If no param specified (empty) in deployStarterKit.json, create a temp name (passed in as $2)
-# EXAMPLE call to this function: AWS_REGION=$(getParam AWS_REGION us-east-1); us-east-1 is passed in as the default value, if none specified and read as $2
-function getParam {
-    DEFVALUE=$(grep $1 $CONFIG_FILE | sed -e 's/.*://;s/"//g;s/,//g')
-    echo $(if test -z $(grep $1 $CONFIG_FILE | sed -e 's/.*://;s/"//g;s/,//g')
-        then echo $2
-        else echo $DEFVALUE
-    fi)
-}
+CONFIG_FILE="DeployProject.conf"
 
-function getParamAllowOrigins {
-    DEFVALUE=$(grep $1 $CONFIG_FILE | sed -e "s/\"$1\": //;s/\"//g;" | sed 's/,*$//g')
-    echo $(if test -z $(grep $1 $CONFIG_FILE | sed -e 's/.*://;s/"//g;s/,//g')
-        then echo $2
-        else echo $DEFVALUE
-    fi)
-}
+# include configuration file
+source $CONFIG_FILE
 
 # Create a random number for repeated automated deployment
 function getRandomValue {
@@ -26,10 +13,6 @@ function getRandomValue {
 echo -e "\n****************************************"
 echo "Initializing application environment variables"
 
-CONFIG_FILE="DeployProject_Settings.json"
-
-RP_ALLOWED_ORIGINS=$(getParamAllowOrigins RP_ALLOWED_ORIGINS "localhost:3000")
-
 # ------------------------------------------------
 # DECLARE SYSTEM VARIABLES BASED ON SETTINGS FILE
 # ------------------------------------------------
@@ -38,13 +21,13 @@ RP_ALLOWED_ORIGINS=$(getParamAllowOrigins RP_ALLOWED_ORIGINS "localhost:3000")
 # Options: custom
 # Denotes the RP ID used by your application
 # ------------------------------------------------
-RP_ID=$(getParam RP_ID localhost)
+RP_ID="${RP_ID:-localhost}"
 
 # ------------------------------------------------
 # Options: custom
 # Denotes the RP NAME used by your application
 # ------------------------------------------------
-RP_NAME=$(getParam RP_NAME "My app")
+RP_NAME="${RP_NAME:-My app}"
 
 # ------------------------------------------------
 # Options: custom
@@ -52,27 +35,27 @@ RP_NAME=$(getParam RP_NAME "My app")
 # Current script allows for one allowed origins, without the protocol heading
 # ex. localhost:3000 will allow both http://localhost:3000 and https://localhost:3000
 # ------------------------------------------------
-RP_ALLOWED_ORIGINS=$(getParamAllowOrigins RP_ALLOWED_ORIGINS "localhost:3000")
+RP_ALLOWED_ORIGINS="${RP_ALLOWED_ORIGINS:-localhost:3000}"
 
 # ------------------------------------------------
 # Options: custom
 # Denotes the origins allowed to access the API (CORS)
 # ex. localhost:3000 will allow both http://localhost:3000 and https://localhost:3000
 # ------------------------------------------------
-RP_ALLOWED_CROSS_ORIGINS=$(getParamAllowOrigins RP_ALLOWED_CROSS_ORIGINS "localhost:3000")
+RP_ALLOWED_CROSS_ORIGINS="${RP_ALLOWED_CROSS_ORIGINS:-localhost:3000}"
 
 # ------------------------------------------------
 # Options: DIRECT, INDIRECT, ENTERPRISE, NONE
 # Values should be all caps to be utilized by java webauthn library
 # ------------------------------------------------
-RP_ATTESTATION_PREFERENCE=$(getParam RP_ATTESTATION_PREFERENCE DIRECT)
+RP_ATTESTATION_PREFERENCE="${RP_ATTESTATION_PREFERENCE:-DIRECT}"
 
 # ------------------------------------------------
 # Options: true/false
 # Will denote if your application will require attstation
 # for every new registration
 # ------------------------------------------------
-RP_ALLOW_UNTRUSTED_ATTESTATION=$(getParam RP_ALLOW_UNTRUSTED_ATTESTATION true)
+RP_ALLOW_UNTRUSTED_ATTESTATION="${RP_ALLOW_UNTRUSTED_ATTESTATION:-true}"
 
 # ------------------------------------------------
 # Options: local
@@ -80,27 +63,27 @@ RP_ALLOW_UNTRUSTED_ATTESTATION=$(getParam RP_ALLOW_UNTRUSTED_ATTESTATION true)
 # to create logic that will change based on your
 # development environment
 # ------------------------------------------------
-DEPLOYMENT_ENVIRONMENT=$(getParam DEPLOYMENT_ENVIRONMENT local)
+DEPLOYMENT_ENVIRONMENT="${DEPLOYMENT_ENVIRONMENT:-local}"
 
 # ------------------------------------------------
 # Options: in-mem, mysql
 # local will leverage in memory data storage, all registrations will be lost when the application is shut down
 # mysql will deploy a local instance of mysql through Docker
 # ------------------------------------------------
-DATABASE_TYPE=$(getParam DATABASE_TYPE mysql)
+DATABASE_TYPE="${DATABASE_TYPE:-mysql}"
 
 # ------------------------------------------------
 # Options: custom string
 # Allow you to set your own database password, or generate a random one
 # ------------------------------------------------
-DATABASE_ROOT_PASSWORD=$(getParam DATABASE_ROOT_PASSWORD $(getRandomValue 16) )
+DATABASE_ROOT_PASSWORD="${DATABASE_ROOT_PASSWORD:-$(getRandomValue 16)}"
 
 # ------------------------------------------------
 # Options: react | none
 # Allow you to choose the client you will be using for testing
 # none - deploys only the java app
 # ------------------------------------------------
-CLIENT_TYPE=$(getParam CLIENT_TYPE react )
+CLIENT_TYPE="${CLIENT_TYPE:-react}"
 
 # ------------------------------------------------
 # Options: keycloak | none
@@ -108,7 +91,7 @@ CLIENT_TYPE=$(getParam CLIENT_TYPE react )
 # none - no IDP deployed
 # keycloak - initializes local instance of keycloak
 # ------------------------------------------------
-IDP_TYPE=$(getParam IDP_TYPE keycloak )
+IDP_TYPE="${IDP_TYPE:-keycloak}"
 
 echo -e "\nVariables initialized"
 echo "****************************************"
