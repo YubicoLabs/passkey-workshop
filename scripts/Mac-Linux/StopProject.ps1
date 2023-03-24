9fff5a97b1fd35c988f2cfe94eb19f34ce671471
Joost van Dijk <joost.vandijk@yubico.com>
2023-03-24 15:11:34 +0100

    add powershell versions of all scripts

---
+++ scripts/Mac-Linux/StopProject.ps1
@@ -0,0 +1,19 @@
+Write-Host "Stopping react app"
+Set-Location -Path react-app
+./stop_react_app.ps1
+Set-Location -Path -
+
+Write-Host "Stopping java app"
+Set-Location -Path java-app
+./stop_java_app.ps1
+Set-Location -Path -
+
+Write-Host "Stopping keycloak"
+Set-Location -Path keycloak
+./stop_keycloak.ps1
+Set-Location -Path -
+
+Write-Host "Stopping database"
+Set-Location -Path mysql
+./stop_mysql.ps1
+Set-Location -Path -
---
+++ scripts/Mac-Linux/keycloak/deploy_keycloak.ps1
@@ -0,0 +1,7 @@
+echo "Composing Keycloak"
+
+if( ![System.IO.File]::Exists("$pwd/build/passkey_authenticator.jar") ) {
+  New-Item -Path . -Name "build" -ItemType "directory"
+  Copy-Item -Path "../../../examples/IdentityProviders/KeyCloak/pre-build/passkey_authenticator.jar" -Destination "./build"
+}
+docker compose up -d
