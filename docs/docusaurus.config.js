22cae705facfb71da75f1adfa51c08128226426a
csalas <c.salas@yubico.com>
2023-04-03 14:52:31 -0500

    Init gh-pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'yubicolabs', // Usually your GitHub org/user name.
-  projectName: 'passkey-relying-party-example', // Usually your repo name.
+  projectName: 'passkey-workshop', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
