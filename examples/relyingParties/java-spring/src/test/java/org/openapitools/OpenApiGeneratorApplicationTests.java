2dfb73b7aa356f1df5e7421a0342475de06f1d98
csalas <c.salas@yubico.com>
2023-04-12 14:12:35 -0500

    Simple test case for validating RP instance identity

--- examples/relyingParties/java-spring/src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
+++ examples/relyingParties/java-spring/src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
@@ -1,20 +1,38 @@
 package org.openapitools;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.yubico.webauthn.RelyingParty;
+import com.yubicolabs.passkey_rp.services.passkey.RelyingPartyInstance;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OpenApiGeneratorApplicationTests {
 
     // Removing test as it requires a live database connection
+    @Autowired
+    RelyingPartyInstance relyingPartyInstance;
 
+    @Autowired
+    Environment env;
+
+    /**
+     * Test the RelyingPartyInstance singleton parameters
+     */
     @Test
-    void test1() {
-        assertTrue(true);
+    void testRPInstanceConfigs() {
+        /**
+         * Test the RP_ID and RP_NAME
+         */
+        assertEquals(env.getProperty("RP_ID"), relyingPartyInstance.getRelyingParty().getIdentity().getId());
+        assertEquals(env.getProperty("RP_NAME"), relyingPartyInstance.getRelyingParty().getIdentity().getName());
     }
 
 }
