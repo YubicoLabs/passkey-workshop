1c1139e06e553b6caacc893a5d71751690022570
Dennis Hills <dennis.hills@yubico.com>
2023-06-21 23:53:25 -0700

    adding new HA database schema

---
+++ scripts/mysql/high_assurance_db.sql
@@ -0,0 +1,62 @@
+USE passkeyStorage;
+DROP TABLE IF EXISTS hibernate_sequence, attestation_requests, assertion_requests, credential_registrations, account_transaction, account, user;
+CREATE TABLE user (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    userName NVARCHAR(50) NOT NULL UNIQUE,
+    displayName NVARCHAR(50),
+    registrationDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    lastUpdatedDate DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    lastLoginDate DATETIME,
+    PRIMARY KEY (id)
+);
+CREATE TABLE account (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_handle NVARCHAR(50) NOT NULL UNIQUE,
+    advanced_protection BOOL,
+    balance DECIMAL(13,2),
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+CREATE TABLE account_transaction (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    account_id BIGINT NOT NULL,
+    type NVARCHAR(50),
+    amount DECIMAL(13,2),
+    status BOOL,
+    transaction_create_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    description TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (account_id) REFERENCES account(id)
+);
+CREATE TABLE attestation_requests (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    attestation_request TEXT,
+    is_active BOOL DEFAULT TRUE,
+    request_id TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE assertion_requests (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    assertion_request TEXT,
+    is_active BOOL,
+    request_id TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE credential_registrations (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    credential TEXT,
+    credentialid TEXT,
+    credential_nickname TEXT,
+    iconuri TEXT,
+    last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    last_used_time BIGINT,
+    registration_time BIGINT,
+    user_handle TEXT, 
+    user_identity TEXT,
+    state NVARCHAR(50), 
+    isHighAssurance BOOL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE hibernate_sequence (
+    next_val BIGINT NOT NULL
+);
