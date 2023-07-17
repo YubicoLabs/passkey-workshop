125eda7158acbe1ddcaa3bd04712f179995c18e2
Joost van Dijk <joost.vandijk@yubico.com>
2023-07-17 16:21:50 +0200

    add OAuth2 protection

---
+++ examples/high_assurance/bank_app/src/main/java/com/yubicolabs/bank_app/api/ResourceServerSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.demo_rp.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * OAuth resource configuration.
+ */
+@Configuration
+@EnableWebSecurity
+public class ResourceServerSecurityConfiguration {
+
+	@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+	String jwkSetUri;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+		.authorizeHttpRequests((authorize) -> authorize
+				.anyRequest().authenticated() // TODO: filter on scopes if necessary
+		)
+		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+		// @formatter:on
+		return http.build();
+	}
+
+	@Bean
+	JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+	}
+
+}
