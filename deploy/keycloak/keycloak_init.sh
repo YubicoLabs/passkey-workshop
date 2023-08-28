f8b4b40baf295b3af0059c6a4e5102f4b62d6e62
Joost van Dijk <joost.vandijk@yubico.com>
2023-08-28 17:10:16 +0200

    run kcadm script on localhost instead of docker host

--- deploy/keycloak/keycloak_init.sh
+++ deploy/keycloak/keycloak_init.sh
@@ -4,7 +4,7 @@ if [ -e init.done ]; then
   exit
 fi
 
-KEYCLOAK_PATH="./opt/keycloak/bin/kcadm.sh"
+KEYCLOAK_PATH="/opt/keycloak/bin/kcadm.sh"
 CONTAINER_NAME=affectionate_carver
 
 REALM_NAME=passkeyDemo
@@ -18,29 +18,29 @@ CLIENT_URL=http://localhost:3000
 [[ -z "$1" ]] || CLIENT_URL=$1
 echo "Configuring keycloak client on $CLIENT_URL"
 
-while ! ./opt/keycloak/bin/kcadm.sh config credentials --server http://host.docker.internal:8081 --realm master --user admin --password admin --client admin-cli &> /dev/null ; do
+while ! /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin --client admin-cli &> /dev/null ; do
     echo 'Waiting for connection'
     sleep 5
 done
 echo "Connection found"
 
-./opt/keycloak/bin/kcadm.sh create realms -s realm=$REALM_NAME -s enabled=true -s registrationAllowed=true
+/opt/keycloak/bin/kcadm.sh create realms -s realm=$REALM_NAME -s enabled=true -s registrationAllowed=true
 
 # Create the Flow for passkey authentication
-AUTH_OUTPUT=$(./opt/keycloak/bin/kcadm.sh create authentication/flows -s alias=$AUTH_FLOW_NAME -s providerId=basic-flow -s topLevel=true -r $REALM_NAME --id)
-./opt/keycloak/bin/kcadm.sh create authentication/flows/$AUTH_FLOW_NAME/executions/execution -b '{"provider": "passkeyauthenticator"}' -r $REALM_NAME
+AUTH_OUTPUT=$(/opt/keycloak/bin/kcadm.sh create authentication/flows -s alias=$AUTH_FLOW_NAME -s providerId=basic-flow -s topLevel=true -r $REALM_NAME --id)
+/opt/keycloak/bin/kcadm.sh create authentication/flows/$AUTH_FLOW_NAME/executions/execution -b '{"provider": "passkeyauthenticator"}' -r $REALM_NAME
 
 # Create the Flow for passkey registration
-REG_OUTPUT=$(./opt/keycloak/bin/kcadm.sh create authentication/flows -s alias=$REG_FLOW_NAME -s providerId=basic-flow -s topLevel=true -r $REALM_NAME --id)
-./opt/keycloak/bin/kcadm.sh create authentication/flows/$REG_FLOW_NAME/executions/execution -b '{"provider": "passkeyregister"}' -r $REALM_NAME
+REG_OUTPUT=$(/opt/keycloak/bin/kcadm.sh create authentication/flows -s alias=$REG_FLOW_NAME -s providerId=basic-flow -s topLevel=true -r $REALM_NAME --id)
+/opt/keycloak/bin/kcadm.sh create authentication/flows/$REG_FLOW_NAME/executions/execution -b '{"provider": "passkeyregister"}' -r $REALM_NAME
 # Set the new registration flow as the default for the realm
-./opt/keycloak/bin/kcadm.sh update realms/$REALM_NAME -s registrationFlow=passkeyRegFlow
+/opt/keycloak/bin/kcadm.sh update realms/$REALM_NAME -s registrationFlow=passkeyRegFlow
 
 echo $AUTH_OUTPUT
 # Setting the browser flow isnt' working - look into this
-echo ./opt/keycloak/bin/kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_NAME -s protocol=openid-connect -s enabled=true -s "redirectUris=[\"$CLIENT_URL/logout\", \"/*\", \"$CLIENT_URL/oidc/callback\"]" -s 'webOrigins=["*", "/*"]' -s 'authenticationFlowBindingOverrides={"browser": "'$AUTH_OUTPUT'", "direct_grant": ""}'
+echo /opt/keycloak/bin/kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_NAME -s protocol=openid-connect -s enabled=true -s "redirectUris=[\"$CLIENT_URL/logout\", \"/*\", \"$CLIENT_URL/oidc/callback\"]" -s 'webOrigins=["*", "/*"]' -s 'authenticationFlowBindingOverrides={"browser": "'$AUTH_OUTPUT'", "direct_grant": ""}'
 # TODO: configure callbacks
-./opt/keycloak/bin/kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_NAME -s protocol=openid-connect -s enabled=true -s "redirectUris=[\"$CLIENT_URL/logout\", \"/*\", \"$CLIENT_URL/oidc/callback\"]" -s 'webOrigins=["*", "/*"]' -s 'authenticationFlowBindingOverrides={"browser": "'$AUTH_OUTPUT'", "direct_grant": ""}'
+/opt/keycloak/bin/kcadm.sh create clients -r $REALM_NAME -s clientId=$CLIENT_NAME -s protocol=openid-connect -s enabled=true -s "redirectUris=[\"$CLIENT_URL/logout\", \"/*\", \"$CLIENT_URL/oidc/callback\"]" -s 'webOrigins=["*", "/*"]' -s 'authenticationFlowBindingOverrides={"browser": "'$AUTH_OUTPUT'", "direct_grant": ""}'
 
 echo "Finalizing initalization"
 > init.done
