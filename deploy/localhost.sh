d9b1daa267b7107b9f4fcb3ec70d649496f0023b
Joost van Dijk <joost.vandijk@yubico.com>
2023-10-10 14:25:14 +0200

    always copy sources when deploying to localhost
    Copying sources will restore all defaults, which assume deployment to localhost
    This prevents issues with stale configs generated during a tunneled deployment

--- deploy/localhost.sh
+++ deploy/localhost.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This script automates the deployment of all components on localhost
-# To make the front and backend available on the Internet (for instance for use on a mobile device), see cloudflared.sh
+# To make the front and backend available on the Internet (for instance for use on a mobile device), see devtunnel.sh
 
 [[ -f .env ]] || cp default.env .env
-[[ -d react-app/source ]] || cp -r ../examples/clients/web/react/passkey-client/ react-app/source/
-[[ -d java-app/source ]] || cp -r  ../examples/relyingParties/java-spring/ java-app/source/
-[[ -d keycloak/source ]] || cp -r ../examples/IdentityProviders/KeyCloak/passkey_authenticator/ keycloak/source/
-docker compose --profile web up -d
+cp -r ../examples/clients/web/react/passkey-client/ react-app/source/
+cp -r ../examples/relyingParties/java-spring/ java-app/source/
+cp -r ../examples/IdentityProviders/KeyCloak/passkey_authenticator/ keycloak/source/
+docker compose up -d
