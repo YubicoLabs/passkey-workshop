0b952e7e5e5d1e8268a596809b038e57f3d52794
Joost van Dijk <joost.vandijk@yubico.com>
2023-11-13 19:59:36 +0100

    fix typo in docker hostname

--- examples/IdentityProviders/KeyCloak/passkey_spi/src/main/java/com/yubicolabs/keycloak/registration/PasskeyRegister.java
+++ examples/IdentityProviders/KeyCloak/passkey_spi/src/main/java/com/yubicolabs/keycloak/registration/PasskeyRegister.java
@@ -29,7 +29,7 @@ public class PasskeyRegister implements Authenticator {
 
   ObjectMapper mapper = new ObjectMapper();
 
-  public static final String DEFAULT_WEBAUTHN_API_URL = "http://host.docker.intetnal:8080/v1";
+  public static final String DEFAULT_WEBAUTHN_API_URL = "http://host.docker.internal:8080/v1";
 
   private static final Logger logger = Logger.getLogger(PasskeyRegister.class);
 
