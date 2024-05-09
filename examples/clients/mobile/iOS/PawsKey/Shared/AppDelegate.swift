084f7bf10b468f162de2bf26da4e92391dbb68bd
Dennis Hills <dennis.hills@yubico.com>
2024-05-08 23:49:30 -0700

    added back workshop sample iOS PawsKey app

---
+++ examples/clients/mobile/iOS/PawsKey/Shared/AppDelegate.swift
@@ -0,0 +1,27 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The app's delegate.
+*/
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    let accountManager = AccountManager()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
+
