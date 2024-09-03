3be904a020f44a9808585744209a92f083fdaeff
Mario Bodemann <mario.bodemann@yubico.com>
2024-09-03 15:18:00 +0200

    android:pawskeys:polish: add initial relying party implementation
    Talking to `/v1/status` only.

--- examples/clients/mobile/android/PawsKey/app/src/main/java/io/yubicolabs/pawskey/ApiModule.kt
+++ examples/clients/mobile/android/PawsKey/app/src/main/java/io/yubicolabs/pawskey/ApiModule.kt
@@ -4,6 +4,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import io.yubicolabs.pawskey.net.DebugInterceptor
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 @InstallIn(ViewModelComponent::class)
 @Module
@@ -12,6 +19,48 @@ object ApiModule {
     fun providePasskeyService(): PassKeyService = PassKeyService()
 
     @Provides
-    fun provideRelyingPartyService(): RelyingPartyService =
-        RelyingPartyService()
+    fun provideRelyingPartyService(
+        relyingPartyHttpService: RelyingPartyHttpService,
+    ): RelyingPartyService =
+        RelyingPartyService(
+            relyingPartyHttpService
+        )
+
+    @Provides
+    fun provideRelyingPartyHttpService(
+        okHttpClient: OkHttpClient,
+        jsonConverter: Converter.Factory,
+    ): RelyingPartyHttpService =
+        Retrofit.Builder()
+            .baseUrl("https://${BuildConfig.API_BASE_URI}/")
+            .client(okHttpClient)
+            .addConverterFactory(jsonConverter)
+            .build()
+            .create(RelyingPartyHttpService::class.java)
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient()
+            .newBuilder()
+
+        if (BuildConfig.DEBUG) {
+            builder.addInterceptor(
+                DebugInterceptor()
+            )
+        }
+
+        return builder.build()
+    }
+
+    private val defaultJson = Json {
+        encodeDefaults = true
+        ignoreUnknownKeys = true
+        prettyPrint = BuildConfig.DEBUG
+    }
+
+    @Provides
+    fun providesJsonFactory(): Converter.Factory = defaultJson
+        .asConverterFactory(
+            MediaType.get("application/json")
+        )
 }
